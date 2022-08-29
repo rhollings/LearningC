2339eb794a3a89b175fc72aba767cd7d6be0e942
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-08-29 09:38:35 +0200

    Create basicMath.c

---
+++ c/basicMath.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+// Square Root
+int main() {
+  printf("%f", sqrt(16));
+  return 0;
+}
+
+// Rounding
+int main() {
+  printf("%f\n", ceil(1.4));
+  printf("%f\n", floor(1.4));
+  return 0;
+}
+
+// To the Power of
+int main() {
+  printf("%f", pow(4, 3));
+  return 0;
+}
+
+/*
+abs(x)	Returns the absolute value of x
+acos(x)	Returns the arccosine of x
+asin(x)	Returns the arcsine of x
+atan(x)	Returns the arctangent of x
+cbrt(x)	Returns the cube root of x
+cos(x)	Returns the cosine of x
+exp(x)	Returns the value of Ex
+sin(x)	Returns the sine of x (x is in radians)
+tan(x)	Returns the tangent of an angle
+*/
