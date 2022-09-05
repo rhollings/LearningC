83cb2dbde869422de5861015bc406df3139a5ee5
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-09-05 10:37:20 +0200

    Create basicMath.cpp

---
+++ c++/basicMath.cpp
@@ -0,0 +1,15 @@
+// The max(x,y) function can be used to find the highest value of x and y:
+cout << max(5, 10);
+
+// And the min(x,y)
+cout << min(5, 10);
+
+// Other functions, such as sqrt (square root), round (rounds a number) and log (natural logarithm), can be found in the <cmath> header file:
+// Include the cmath library
+#include <cmath>
+
+cout << sqrt(64);
+cout << round(2.6);
+cout << log(2);
+
+// https://www.w3schools.com/cpp/cpp_math.asp 
