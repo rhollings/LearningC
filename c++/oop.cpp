6a90c05c524f4ae5642660ef476dc7747815ad17
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-10-04 11:56:26 -0600

    Update oop.cpp

--- c++/oop.cpp
+++ c++/oop.cpp
@@ -114,3 +114,47 @@ int main() {
   cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
   return 0;
 }
+
+
+// Encapsulation
+/*
+The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. 
+To achieve this, you must declare class variables/attributes as private (cannot be accessed from outside the class).
+If you want others to read or modify the value of a private member, you can provide public get and set methods.
+*/
+
+#include <iostream>
+using namespace std;
+
+class Employee {
+  private:
+    // Private attribute
+    int salary;
+
+  public:
+    // Setter
+    void setSalary(int s) {
+      salary = s;
+    }
+    // Getter
+    int getSalary() {
+      return salary;
+    }
+};
+
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  cout << myObj.getSalary();
+  return 0;
+}
+
+/*
+The salary attribute is private, which have restricted access.
+
+The public setSalary() method takes a parameter (s) and assigns it to the salary attribute (salary = s).
+
+The public getSalary() method returns the value of the private salary attribute.
+
+Inside main(), we create an object of the Employee class. Now we can use the setSalary() method to set the value of the private attribute to 50000. Then we call the getSalary() method on the object to return the value.
+*/
