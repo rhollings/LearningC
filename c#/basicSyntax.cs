b9b9ea24c3957c4fdaea740b92eec9bf3345bf62
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-10-24 10:14:00 -0600

    Update basicSyntax.cs

--- c#/basicSyntax.cs
+++ c#/basicSyntax.cs
@@ -44,3 +44,12 @@ bool myBool = true;
 string myText = "Hello";
 
 
+// User Input
+// Type your username and press enter
+Console.WriteLine("Enter username:");
+
+// Create a string variable and get user input from the keyboard and store it in the variable
+string userName = Console.ReadLine();
+
+// Print the value of the variable (userName), which will display the input value
+Console.WriteLine("Username is: " + userName);
