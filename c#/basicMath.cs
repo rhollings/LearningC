37db9e9bb640333528323d5b60d449024443281d
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-10-24 10:16:43 -0600

    Create basicMath.cs

---
+++ c#/basicMath.cs
@@ -0,0 +1,16 @@
+// The Math.Max(x,y) method can be used to find the highest value of x and y:
+Math.Max(5, 10);
+
+// The Math.Min(x,y) method can be used to find the lowest value of of x and y:
+Math.Min(5, 10);
+
+// The Math.Sqrt(x) method returns the square root of x:
+Math.Sqrt(64);
+
+// The Math.Abs(x) method returns the absolute (positive) value of x:
+Math.Abs(-4.7);
+
+// Math.Round() rounds a number to the nearest whole number:
+Math.Round(9.99);
+
+
