15bcb4f77beb5c196d8c02b098c24f2b712d631a
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-10-26 15:02:34 -0600

    Update basicStrings.cs

--- c#/basicStrings.cs
+++ c#/basicStrings.cs
@@ -10,6 +10,18 @@ Console.WriteLine(myString[0]);  // Outputs "H"
 string myString = "Hello";
 Console.WriteLine(myString.IndexOf("e"));  // Outputs "1"
 
+// The + operator can be used between strings to combine them. This is called concatenation:
+string firstName = "John ";
+string lastName = "Doe";
+string name = firstName + lastName;
+Console.WriteLine(name);
+
+// You can also use the string.Concat() method to concatenate two strings:
+string firstName = "John ";
+string lastName = "Doe";
+string name = string.Concat(firstName, lastName);
+Console.WriteLine(name);
+
 // Full name
 string name = "John Doe";
 
