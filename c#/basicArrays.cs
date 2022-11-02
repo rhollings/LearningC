9c1dc75959f002aee20f2dedcacc0df6f43d4dc1
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-11-01 21:17:44 -0600

    Create basicArrays.cs
    init new file

---
+++ c#/basicArrays.cs
@@ -0,0 +1,8 @@
+/*
+Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+To declare an array, define the variable type with square brackets:
+*/
+
+string[] cars;
+
