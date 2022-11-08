264100a65bbbd25c9e1ff939775c642b8dcdaf25
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-11-07 22:26:00 -0700

    Update basicArrays.cs

--- c#/basicArrays.cs
+++ c#/basicArrays.cs
@@ -40,3 +40,16 @@ string[] cars = new string[] {"Volvo", "BMW", "Ford", "Mazda"};
 
 // Create an array of four elements, omitting the new keyword, and without specifying the size
 string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+/*
+Loop Through an Array
+You can loop through the array elements with the for loop, and use the Length property to specify how many times the loop should run.
+
+The following example outputs all elements in the cars array:
+*/
+
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+for (int i = 0; i < cars.Length; i++) 
+{
+  Console.WriteLine(cars[i]);
+}
